421b1b120708e5eeeb40228b8bf53abcaca36a7c
Katelyn Vi <katelynvi@gmail.com>
2026-01-26 15:18:19 -0800

    Update dashboard such that component skeletons are loaded upon page request and the data is streamed once component is loaded on server. Suspend components and data fetching so that elements within page are loaded as a group, not one by one

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -16,12 +16,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Revenue data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -37,12 +37,18 @@ export async function fetchLatestInvoices() {
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
-      LIMIT 7`;
+      LIMIT 5`;
+
+    console.log('Fetching latest invoice data...');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+    
+    console.log('Invoice data fetch completed after 1 second.');
+
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
